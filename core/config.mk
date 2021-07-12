StatiXOS/android_build

build: move custom includes up
Change-Id: I53334bc8ea67ba850f3589b5a19d7131e4725140

core/config.mk
@@ -294,6 +294,10 @@ include $(BUILD_SYSTEM)/envsetup.mk
 # See envsetup.mk for a description of SCAN_EXCLUDE_DIRS
 FIND_LEAVES_EXCLUDES := $(addprefix --prune=, $(SCAN_EXCLUDE_DIRS) .repo .git)
 
+ifneq ($(STATIX_BUILD),)
+include vendor/statix/config/BoardConfigStatix.mk
+endif
+
 # The build system exposes several variables for where to find the kernel
 # headers:
 #   TARGET_DEVICE_KERNEL_HEADERS is automatically created for the current
@@ -1232,7 +1236,11 @@ dont_bother_goals := out \
     product-graph dump-products
 
 ifneq ($(STATIX_BUILD),)
-include vendor/statix/config/BoardConfigStatix.mk
+ifneq ($(wildcard device/statix/sepolicy/common/sepolicy.mk),)
+## We need to be sure the global selinux policies are included
+## last, to avoid accidental resetting by device configs
+$(eval include device/statix/sepolicy/common/sepolicy.mk)
+endif
 endif
 
 ifeq ($(CALLED_FROM_SETUP),true)
@@ -1244,12 +1252,6 @@ endif
 -include external/ltp/android/ltp_package_list.mk
 DEFAULT_DATA_OUT_MODULES := ltp $(ltp_packages) $(kselftest_modules)
 .KATI_READONLY := DEFAULT_DATA_OUT_MODULES
-
-ifneq ($(STATIX_BUILD),)
-## We need to be sure the global selinux policies are included
-## last, to avoid accidental resetting by device configs
-$(call inherit-product-if-exists, device/statix/sepolicy/common/sepolicy.mk)
-endif
 
 include $(BUILD_SYSTEM)/dumpvar.mk
 

ProjektEva/platform_build

core/config.mk
@@ -294,6 +294,10 @@ include $(BUILD_SYSTEM)/envsetup.mk
 # See envsetup.mk for a description of SCAN_EXCLUDE_DIRS
 FIND_LEAVES_EXCLUDES := $(addprefix --prune=, $(SCAN_EXCLUDE_DIRS) .repo .git)
 
+ifneq ($(EVA_BUILD),)
+include vendor/eva/config/BoardConfigEva.mk
+endif
+
 # The build system exposes several variables for where to find the kernel
 # headers:
 #   TARGET_DEVICE_KERNEL_HEADERS is automatically created for the current
@@ -1230,7 +1234,11 @@ dont_bother_goals := out \
     product-graph dump-products
 
 ifneq ($(EVA_BUILD),)
-include vendor/eva/config/BoardConfigEva.mk
+ifneq ($(wildcard device/eva/sepolicy/common/sepolicy.mk),)
+## We need to be sure the global selinux policies are included
+## last, to avoid accidental resetting by device configs
+$(eval include device/eva/sepolicy/common/sepolicy.mk)
+endif
 endif
 
 ifeq ($(CALLED_FROM_SETUP),true)
@@ -1242,12 +1250,6 @@ endif
 -include external/ltp/android/ltp_package_list.mk
 DEFAULT_DATA_OUT_MODULES := ltp $(ltp_packages) $(kselftest_modules)
 .KATI_READONLY := DEFAULT_DATA_OUT_MODULES
-
-ifneq ($(EVA_BUILD),)
-## We need to be sure the global selinux policies are included
-## last, to avoid accidental resetting by device configs
-$(eval include device/eva/sepolicy/common/sepolicy.mk)
-endif
 
 include $(BUILD_SYSTEM)/dumpvar.mk
 